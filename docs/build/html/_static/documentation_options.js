9c7a3949ebfa1ea222695bf0146794ead36ba0bf
joelelliottsdh <jdelliott@mun.ca>
2021-03-24 16:45:47 -0230

    First Addition to Advanced Guide (Welcome, Change, Support)

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.0.1',
@@ -10,17 +9,4 @@ var DOCUMENTATION_OPTIONS = {
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
-=======
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    BUILDER: 'html',
-    FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
->>>>>>> c51b3ce46a279a7ab250f584039eb5661860edc3
 };
